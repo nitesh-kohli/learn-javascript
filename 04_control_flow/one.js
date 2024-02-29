2e19a5321cf67b165ab1e4b5a0ded3479b7a46f9
Nitesh Kohli <niteshkohli015@gmail.com>
2024-02-29 09:35:31 +0000

    if else added

---
+++ 04_control_flow/one.js
@@ -0,0 +1,13 @@
+if(2 == '2'){
+    console.log("executed");
+}
+
+if(2 === '2'){
+    console.log("executed2");
+}
+
+const score = 200
+if(score > 100){
+    const power = "fly"
+    console.log(`user power is: ${power}`);
+}
